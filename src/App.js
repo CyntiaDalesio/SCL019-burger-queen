ab1d4759bb92fe487ec8db30db212d7ee90fcfd2
CyntiaDalesio <cyntiam.135@gmail.com>
2022-05-04 12:21:06 -0400

    useContent for lunch and breakfast

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import { Routes, Route } from 'react-router-dom';
 import Principal from './pages/Principal';
 import Menu from './pages/Menu';
 import Error from './pages/Error';
-
+import {UseStates} from './Usecontext/UserContent'
+import React from 'react';
 
 function App() {
   return (
-    <Routes>
+    <UseStates>
+   <Routes>
       <Route path='/' element={<Principal />} />
       <Route path='/Menu' element={ <Menu />} />
       <Route path='*' element={<Error />} />
-
-
     </Routes>
+    </UseStates>
+ 
   );
 }
 
--- src/components/Lunch.js
+++ src/components/Lunch.js
@@ -1,26 +1,58 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import data from '../data/menus.json';
-import Add from './Add';
-import Delete from './Delete';
+// import Add from './Add';
+// import Delete from './Delete';
+import {UserContent} from '../Usecontext/UserContent'
+
 function Lunch(props) {
-    const select = (id) =>{
+
+   const {cart,setCart} = useContext(UserContent);
+
+    const select = (id,price,name) =>{
         console.log('el id es:',id);
+
+
     }
+
+    // function addToCart(item) {
+    //     setCart([...cart, { ...item }]);
+    //     console.log("estamos dando click a los elementos");
+    // }
+
+    const addcart = (id,price,name) =>{
+
+        setCart([...cart,{ id: id, name: name, price: price }]
+            );
+      
+
+        // setCart();
+        console.log();
+    }
+
     return (
         <div>
             <span>
-
+            <table className="table table-dark table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="name">Nombre</th>
+                            <th scope="price">Precio</th>
+                        </tr>
+                    </thead>
+                    <tbody>
                 {data.lunch.map(element => {
 
                     return (
-                        <div className="">
-                            
-                            <li className='list-group-item list-group-item-action' onClick={() =>select(element.id)}>{element.name} ------- {element.price} </li>
-                            
-                        </div>
+                        <tr key={element.id} >
+                                    <td onClick={() =>addcart(element.id,element.price,element.name)}> {element.name} </td>
+                                    <td>$ {element.price}</td>
+                                </tr>
+
                     )
 
                 })}
+                 </tbody>
+                </table>
             </span>
         </div>
     );
